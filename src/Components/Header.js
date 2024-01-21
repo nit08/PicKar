53fdd6085a532dd9bc3e3e7415953ef680a0fd34
Nitant <nitantgupta08@gmail.com>
2024-01-21 15:44:47 +0530

    login form

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,44 +1,85 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
+import ReactModal from "react-modal";
+import LoginForm from "./Login/LoginForm";
 
 export default function Header() {
   const router = useRouter();
+  const [login, setlogin] = useState(false);
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        padding: "50px 10px",
-      }}
-    >
-      <header
-        className="fieldCont"
+    <>
+      <div
         style={{
-          width: "100%",
-          maxWidth: "1200px",
           display: "flex",
-          justifyContent: "space-between",
+          justifyContent: "center",
           alignItems: "center",
+          padding: "50px 10px",
         }}
       >
-        <div className="buttonSec" onClick={() => router.push("/")}>
-          Homepage
-        </div>
-        <div
+        <header
+          className="fieldCont"
           style={{
+            width: "100%",
+            maxWidth: "1200px",
             display: "flex",
+            justifyContent: "space-between",
             alignItems: "center",
-            justifyContent: "space-around",
-            gap: "20px",
           }}
         >
-          <div className="buttonSec">Login</div>
-          <button>Create Account</button>
-        </div>
-      </header>
-    </div>
+          <div className="buttonSec" onClick={() => router.push("/")}>
+            Homepage
+          </div>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-around",
+              gap: "20px",
+            }}
+          >
+            <div className="buttonSec" onClick={() => setlogin(true)}>
+              Login
+            </div>
+            <button>Create Account</button>
+          </div>
+        </header>
+      </div>
+      <ReactModal
+        isOpen={login}
+        onRequestClose={() => setlogin(false)}
+        style={modalStyles}
+        ariaHideApp={false}
+      >
+        <LoginForm onClose={() => setlogin(false)} />
+      </ReactModal>
+    </>
   );
 }
+const modalStyles = {
+  overlay: {
+    position: "fixed",
+    zIndex: 1020,
+    top: 0,
+    left: 0,
+    width: "100vw",
+    height: "100vh",
+    background: "rgba(0, 0, 0, 0.50)",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  content: {
+    background: "white",
+    // width: "80%",
+    maxWidth: "calc(100vw - 2rem)",
+    // maxWidth:"90%",
+    maxHeight: "calc(100vh - 2rem)",
+    overflowY: "auto",
+    position: "relative",
+    border: "1px solid #ccc",
+    borderRadius: "8px",
+    padding: "0px",
+  },
+};
